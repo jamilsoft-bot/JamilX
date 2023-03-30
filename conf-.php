544045c1ed381ad532cfd70af2e7b1a6a4d8de2f
Jamilsoft_Dynamics <myakububauchi@gmail.com>
2023-03-30 01:47:59 +0100

    fix bugs

---
+++ conf-.php
@@ -0,0 +1,16 @@
+<?php
+
+$INDEX = "about";
+$CONF_DIR = "system/configs";
+$CONF_APPS_DIR = "Apps/";
+$CONF_SETTING = "system/configs/setting.json";
+$CONF_SERVICE_DIR = "services/";
+
+
+$DB_Data = [
+    "DB_Host" => "localhost",
+    "DB_User" => "root",
+    "DB_Pass" => "",
+    "DB_Name" => "jamilx",
+
+];
